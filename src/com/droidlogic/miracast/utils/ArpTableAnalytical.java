d7ef798a1c5445f60f3d52773ffa878d5760e084
junlu.jiang <junlu.jiang@amlogic.com>
2023-04-03 15:47:54 +0800

    Miracast: Go [1/1]
    PD#TV-76998
    PD#SWPL-118269
    
    Problem:
    aprtable reads the file earlier than the file update time resulting in
    no useful information being read.
    
    Solution:
    Increase the number of repeat reads.
    
    Verify:
    androidR-local
    
    Change-Id: I3486de11b082a1f9de2d27f5abcd3f354ccf777b
    Signed-off-by: junlu.jiang <junlu.jiang@amlogic.com>
    (cherry picked from commit be5bc0e530b4e3ca49000fa357df8b1625918252)

--- src/com/droidlogic/miracast/utils/ArpTableAnalytical.java
+++ src/com/droidlogic/miracast/utils/ArpTableAnalytical.java
@@ -8,19 +8,20 @@ import java.io.IOException;
 import android.util.Log;
 
 public class ArpTableAnalytical {
-    private static final String TAG = "ArpTable";
+    private static final String TAG = "WifiDirectArpTable";
     private static final String ARP_TABLE_PATH = "/proc/net/arp";
     private static final String INVALID_MAC = "00:00:00:00:00:00";
     private static boolean mIsRunning = false;
 
-    private static final long ARP_GET_TIMEOUT = 15_000L;
-    public static final long ARP_GET_INTERVAL  =200;
-    private long mElapsedTime = 0L;
+    //Not read when re-entered 60 times, each delay of 100 milliseconds more than that is the timeout.
+    private static final long ARP_GET_TIMEOUT = 60;
+    public static final long ARP_GET_INTERVAL  =100;
 
     public static void getMatchedIp(String macAddress, String deviceType, ArpTableMatchListener l) {
         if (!mIsRunning) {
             mIsRunning = true;
-            new ArpTableAnalytical().matchingIp(macAddress, deviceType, l);
+            ArpTableAnalytical arpTableAnalytical = new ArpTableAnalytical();
+            arpTableAnalytical.matchingIp(macAddress, deviceType, l);
         } else {
             Log.w(TAG, "getMatchedIp is running, abort");
         }
@@ -45,62 +46,56 @@ public class ArpTableAnalytical {
 
     private void doMatchUnchecked(String macAddress, String deviceType, ArpTableMatchListener l) {
         boolean isMatched = false;
-        BufferedReader reader = null;
-        try {
-            reader = new BufferedReader(new FileReader(ARP_TABLE_PATH));
-            String line = reader.readLine();
-            Log.d(TAG, "!!!! reader; line=" + line);
+        int retryCount = 0;  // Number of current attempts
 
-            while (true) {
-                line = reader.readLine();
-                if (line != null) {
-                    Log.d(TAG, "enter reader; line=" + line);
-                    String[] tokens = line.split("[ ]+");
-                    if (tokens.length < 6) {
-                        continue;
-                    }
-
-                    String ip = tokens[0];
-                    String mac = tokens[3];
-                    String device = tokens[5];
-                    Log.d(TAG, "enter reader ip= " + tokens[0] + " mac = " + tokens[3]);
-                    if (!INVALID_MAC.equals(mac) && deviceType.equals(device)) {
-                        Log.d(TAG, "enter macAddress.equals");
-                        l.onMatched(ip);
-                        isMatched = true;
-                        break;
-                    }
-                } else if (mElapsedTime < ARP_GET_TIMEOUT){
-                    Log.e(TAG, "Waiting for arp updating");
-                    try {
-                        Thread.sleep(ARP_GET_INTERVAL);
-                        mElapsedTime += ARP_GET_INTERVAL;
-                    } catch (InterruptedException e) {
-                        //...
-                    }
-                } else {
-                    break;
-                }
-            }
-
-            Log.e(TAG, "out read!!!!!!");
-        } catch (FileNotFoundException e) {
-            Log.e(TAG, "Could not open /proc/net/arp to lookup mac address");
-        } catch (IOException e) {
-            Log.e(TAG, "Could not read /proc/net/arp to lookup mac address");
-        } finally {
+        while (retryCount < ARP_GET_TIMEOUT && !isMatched) {
+            BufferedReader reader = null;
             try {
-                if (reader != null) {
-                    reader.close();
+                reader = new BufferedReader(new FileReader(ARP_TABLE_PATH));
+                String line = reader.readLine();
+                Log.d(TAG, "!!!! reader; line=" + line);
+
+                while ((line = reader.readLine()) != null) {
+                        Log.d(TAG, "enter reader; line=" + line);
+                        String[] tokens = line.split("[ ]+");
+                        String ip = tokens[0];
+                        String mac = tokens[3];
+                        String device = tokens[5];
+                        Log.d(TAG, "enter reader ip= " + tokens[0] + " mac = " + tokens[3]);
+                        if (tokens.length >= 6 && !INVALID_MAC.equals(mac) &&  deviceType.equals(device)) {
+                            Log.d(TAG, "enter macAddress.equals");
+                            l.onMatched(ip);
+                            isMatched = true;
+                            break;
+                        }  else {
+                            Log.d(TAG, "Current row does not exist");
+                        }
+                }
+
+                Log.e(TAG, "out read!!!!!!");
+            } catch (FileNotFoundException e) {
+                Log.e(TAG, "Could not open /proc/net/arp to lookup mac address");
+            } catch (IOException e) {
+                Log.e(TAG, "Could not read /proc/net/arp to lookup mac address");
+            } finally {
+                try {
+                    if (reader != null) {
+                        reader.close();
+                        try {
+                            Thread.sleep(ARP_GET_INTERVAL);
+                            retryCount ++;
+                        } catch (InterruptedException e) {
+                            //...
+                        }
+                    }
+                } catch (IOException e) {
+                    // Do nothing
                 }
             }
-            catch (IOException e) {
-                // Do nothing
-            }
-        }
 
-        if (!isMatched) {
-            l.onNotMatched();
+            if (!isMatched) {
+                l.onNotMatched();
+            }
         }
     }
 
